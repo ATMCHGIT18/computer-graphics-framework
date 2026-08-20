0a0d1c289d5e9f4781dd91be499e95d42152bedb
ATMCHGIT18 <torkamanchehaligit@gmail.com>
2026-08-20 13:15:13 +0200

    updated

--- test.cpp
+++ test.cpp
@@ -6,6 +6,8 @@ int main(int argc, char** argv) {
 	float font_size = 1.0f;
 
     bool is_engine_child = (argc > 1 && std::string(argv[1]) == "--engine-child");
+    cgf::Image image = cgf::ImageLoader::load("./FrameWork/src/spirit_test.png");
+    std::cout << "Image size is: " << image.get_width() << " X "<< image.get_height() << std::endl;
 
     if (!is_engine_child) {
         char self_path[4096];
@@ -19,7 +21,7 @@ int main(int argc, char** argv) {
 
     cgf::Window window(height, width, font_size, "test");
     window.get_pixels().clear();
-    cgf::Image image(50,50,cgf::Colors::White);
+    // cgf::Image image(50,50,cgf::Colors::White);
     cgf::SampleBuffer samp(50,50,4);
     cgf::Point2D p1(10,45);
     cgf::Point2D p2(250,300);
@@ -34,8 +36,8 @@ int main(int argc, char** argv) {
     	}
     }
 
-    cgf::Renderer render(window,image);
-    render.render(150,150);
+    cgf::Renderer render(window.get_pixels());
+    render.render(image,150,150);
     line.render(window,cgf::Colors::Blue);
     window.show();
     std::cin.ignore();
