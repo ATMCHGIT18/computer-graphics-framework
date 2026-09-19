06805f9919ed80762a6a41057b46ebba25bb27ed
ATMCHGIT18 <torkamanchehaligit@gmail.com>
2026-09-19 23:52:42 +0200

    Create a small test for X11 graphics and events

--- FrameWork/src/X11 debug/X11_test.cpp
+++ FrameWork/src/X11 debug/X11_test.cpp
@@ -19,32 +19,55 @@ int main(){
 
     rect.render_fill_flood(img,cgf::Colors::Red,cgf::Colors::Green);
 
+    // Cursor cursor = XCreateFontCursor(window.get_display(),84);
+
+    // window.assign_cursor(cursor);
 
 	// render.render(p1,sprit);
-	
-	while (true){
-	    XEvent event;
-	    XNextEvent(window.display,&event);
+    cgf::Event_X11 event;
+    while(true){
+    	std::cout << window.poll_event(event) << std::endl;
+		while (window.poll_event(event)){
 
-	    switch(event.type){
-	    case KeyPress:
-	    	std::cout << "Key Pressed" << std::endl;
-	    	break;
-	    case KeyRelease:
-	    	std::cout << "Key released!" << std::endl;
-	    	break;
-	    case ButtonPress:
-	    	std::cout << "Mouse pressed" << std::endl;
-	    	break;
-	    case ButtonRelease:
-	    	std::cout << "Mouse released!" << std::endl;
-	    	std::cout << "X: "<< event.xmotion.x << " Y: " << event.xmotion.y << std::endl;
-	    	break;
-	    case Expose:
-	    	std::cout << "Exposure Event" << std::endl;
-	    	render.render(p,sprit);
-	    	break;
-	    }
+			if(event.inside_window){ // Making sure that event system is only works when we are actually inside the window ( we will have different idea for making window active or not)
+			    switch(event.type){
+			    case cgf::EventType_X11::KeyPressed:
+			    	std::cout << "Key Pressed" << std::endl;
+			    	std::cout << event.key_code << std::endl;
+			    	if (event.key_code == 39){
+			    		window.get_pixels().clear();
+			    		render.render(p,sprit);
+			    	}
+			    	else if (event.key_code == 40){
+			    		window.get_pixels().clear();
+			    		render.render(cgf::Point2D{400,300},img);
+			    	}
+			    	else if (event.key_code == 114){
+			    		window.get_pixels().clear();
+			    		render.render(p+10,sprit);
+			    	}
+			    	break;
+
+			    case cgf::EventType_X11::KeyReleased:
+			    	std::cout << "Key released!" << std::endl;
+			    	break;
+			    case cgf::EventType_X11::MouseButtonPressed:
+			    	std::cout << "Mouse pressed" << std::endl;
+			    	break;
+			    case cgf::EventType_X11::MouseButtonReleased:
+			    	std::cout << "Mouse released!" << std::endl;
+			    	std::cout << "X: "<< event.mouse_x << " Y: " << event.mouse_y << std::endl;
+			    	break;
+			    case cgf::EventType_X11::Exposed:
+			    	std::cout << "Exposure Event" << std::endl;
+			    	break;
+			    case cgf::EventType_X11::Resized:
+			    	std::cout << "Resized Event" << std::endl;
+			    	std::cout << "X: "<< event.width << " Y: " << event.height << std::endl;
+			    	break;
+			    }
+			}
+		}
 	}
 	// window.createWindow();
 	return 0;
